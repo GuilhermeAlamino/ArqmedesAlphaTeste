a492899205691b642ad8e54606930deb01d88e07
GuilhermeAlamino <guilhermedevelopedweb@hotmail.com>
2023-08-08 05:23:05 -0300

    create ddl

--- .docker/mysql/00-init-db.sql
+++ .docker/mysql/00-init-db.sql
@@ -1,1 +1,30 @@
 -- DDL de criação das tabelas do banco de dados
+
+CREATE DATABASE IF NOT EXISTS arqmedes_alpha_test;
+
+USE arqmedes_alpha_test;
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(20) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    sku VARCHAR(50) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product_category (
+    product_id INT,
+    category_id INT,
+    PRIMARY KEY (product_id, category_id),
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
